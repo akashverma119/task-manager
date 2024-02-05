1d3d042e25acf179b395dc2b3f18cd7d093f1d38
Akash Verma <akash.verma@capillarytech.com>
2024-02-05 13:27:03 +0530

    testing file

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const item = screen.getByRole('link', {
-    name: /task manager app/i
+describe("List page", ()=>{
+  it('navbar task manager is visible', () => {
+    render(<App />);
+    const item = screen.getByRole('link', {
+      name: /task manager app/i
+    });
+    expect(item).toBeInTheDocument();
   });
-  expect(item).toBeInTheDocument();
-});
+  
+  it('navbar task manager is visible', () => {
+    render(<App />);
+    const item = screen.getByRole('link', {
+      name: /create/i
+    });
+    expect(item).toBeInTheDocument();
+  });
+
+  it('task header is present', () => {
+    render(<App />);
+    const item = screen.getByRole('heading', {
+      name: /your daily tasks/i
+    })
+    expect(item).toBeInTheDocument();
+  });
+
+  it('task table header is present', async() => {
+    render(<App />);
+    const title = await screen.findByRole('columnheader', {
+      name: /title/i
+    });
+    const priority = await screen.findByRole('columnheader', {
+      name: /priority/i
+    });
+    const status = await screen.findByRole('columnheader', {
+      name: /status/i
+    });
+
+    expect(title).toBeInTheDocument();
+    expect(priority).toBeInTheDocument();
+    expect(status).toBeInTheDocument();
+  });
+})
+
